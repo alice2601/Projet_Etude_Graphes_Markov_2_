d4a678c3584e3f5ade350f0031b6b2e6e990bc7e
khela <melania.khela@efrei.net>
2025-11-07 21:45:09 +0100

    PARTIE 1 FINI

---
+++ cmake-build-debug/hasse.h
@@ -0,0 +1,18 @@
+//
+// Created by khela on 07/11/2025.
+//
+
+#ifndef __HASSE_H__
+#define __HASSE_H__
+
+//void removeTransitiveLinks(t_link_array *p_link_array);
+
+/**
+ * @brief Creates a link array from the given partition and graph.
+ *
+ * @param part The partition of the graph.
+ * @param graph The adjacency list representation of the graph.
+ * @return The created link array.
+ */
+
+#endif
---
+++ cmake-build-debug/utils.h
@@ -0,0 +1,13 @@
+//
+// Created by khela on 07/11/2025.
+//
+
+
+
+
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+
+#endif
+
